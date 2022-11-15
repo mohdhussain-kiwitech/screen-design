aa46c9243e97b84faedb22606cef7c12fb982372
mohdhussain-kiwitech <mohd.hussain@kiwitech.com>
2022-11-15 12:47:31 +0530

    initial commit

---
+++ src/components/Materialss.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { CardContent, Typography, withStyles } from "@material-ui/core";
+import { sessionStyle } from "./style";
+import ListAltOutlinedIcon from "@material-ui/icons/ListAltOutlined";
+import DateRangeOutlinedIcon from "@material-ui/icons/DateRangeOutlined";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import EvStationIcon from "@material-ui/icons/EvStation";
+import FlashOnOutlinedIcon from "@material-ui/icons/FlashOnOutlined";
+import AttachMoneyOutlinedIcon from "@material-ui/icons/AttachMoneyOutlined";
+import Card from "@material-ui/core/Card";
+import imgs from "../assests/translator.png";
+const Materialss = ({ classes }) => {
+  return (
+    <Card className={classes.root} variant="outlined">
+      <Typography className={classes.title}>
+        <ListAltOutlinedIcon className={classes.titleIcon} />
+        Charging Summary
+        <img src={imgs} alt="" className={classes.logo} />
+      </Typography>
+      <CardContent className={classes.head}>
+        <DateRangeOutlinedIcon className={classes.date} />
+        <Typography className={classes.data}>
+          May 30 , 2022 1:00 PM 4:32 PM
+        </Typography>
+        <Typography className={classes.secondData}> 3 hr 32 min</Typography>
+        <Typography className={classes.thirdData}>Time</Typography>
+        <LocationOnIcon className={classes.icon} />
+        <Typography className={classes.data}>Parkland on Eglinton</Typography>
+        <Typography className={classes.secondData}>20 Howard St</Typography>
+        <Typography className={classes.thirdData}>Location</Typography>
+        <EvStationIcon className={classes.icon} />
+        <Typography className={classes.data}>Parkland-B</Typography>
+        <Typography className={classes.secondData}> 7 kW AC</Typography>
+        <Typography className={classes.thirdData}>Charger</Typography>
+        <FlashOnOutlinedIcon className={classes.icon} />
+        <Typography className={classes.data}>8.2488 kwh</Typography>
+        <Typography className={classes.thirdData}>Energy Added </Typography>
+        <AttachMoneyOutlinedIcon className={classes.icon} />
+        <Typography className={classes.data}>$10.45</Typography>
+        <Typography className={classes.thirdData}>Total</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default withStyles(sessionStyle)(Materialss);
---
+++ src/components/style.js
@@ -0,0 +1,68 @@
+export const sessionStyle = {
+  root: {
+    borderTop: "10px solid #F7F7F7",
+    borderBottom: "10px solid #F7F7F7",
+    padding: "16px 30px",
+  },
+  head: {
+    marginTop: "1rem",
+    width: "33%",
+    marginLeft: "36rem",
+    border: "1px solid #D3D3D3",
+    borderRadius: "1rem",
+  },
+  title: {
+    display: "flex",
+    fontSize: "18px",
+    fontWeight: 600,
+    lineHeight: "32px",
+    justifyContent: "center",
+    marginLeft: "166px",
+  },
+  titleIcon: {
+    fontSize: "2rem",
+    marginRight: "5px",
+    paddingRight: "5px",
+    color: "#525252",
+  },
+  date: {
+    fontSize: "2rem",
+    color: "#525252",
+    marginLeft: "-25rem",
+  },
+  icon: {
+    fontSize: "2rem",
+    color: "#525252",
+    marginTop: "2rem",
+    marginLeft: "-25rem",
+  },
+  margins: {
+    marginTop: "-1rem",
+  },
+  data: {
+    display: "flex",
+    justifyContent: "left",
+    marginTop: "-2rem",
+    marginLeft: "3rem",
+    fontSize: 18,
+    fontWeight: 600,
+  },
+  secondData: {
+    display: "flex",
+    justifyContent: "left",
+    marginLeft: "3rem",
+    fontSize: 18,
+    width: "100%",
+  },
+  thirdData: {
+    display: "flex",
+    justifyContent: "left",
+    marginLeft: "3rem",
+    width: "100%",
+    color: "#808080",
+  },
+  logo: {
+    width: "77px",
+    marginLeft : "5rem"
+  },
+};
